85bdf1766ffeded5189d3a8a4aa61b224c4fa73c
2047 <2047@users.noreply.github.com>
2017-01-23 08:29:32 +0800

    Small fix " › "

--- js/all.js
+++ js/all.js
@@ -2301,9 +2301,9 @@
         username_match = body.match(/^(([a-zA-Z0-9\.]+)@[a-zA-Z0-9\.]+|@(.*?)):/);
         if (username_match) {
           if (username_match[2]) {
-            username_formatted = username_match[2] + " > ";
+            username_formatted = username_match[2] + " › ";
           } else {
-            username_formatted = username_match[3] + " > ";
+            username_formatted = username_match[3] + " › ";
           }
           body = body.replace(/> \[(.*?)\].*/g, "$1: ");
           body = body.replace(/^[ ]*>.*/gm, "");
