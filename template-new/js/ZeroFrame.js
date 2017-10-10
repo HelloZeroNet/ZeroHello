d8ca246158d978ddbd4dbc092439f41c45fc6597
shortcutme <tamas@zeronet.io>
2017-10-10 14:38:57 +0200

    Fix cmdp response when there is no result

--- template-new/js/ZeroFrame.js
+++ template-new/js/ZeroFrame.js
@@ -75,7 +75,7 @@ class ZeroFrame {
     cmdp(cmd, params={}) {
         return new Promise((resolve, reject) => {
             this.cmd(cmd, params, (res) => {
-                if (res.error) {
+                if (res && res.error) {
                     reject(res.error)
                 } else {
                     resolve(res)
