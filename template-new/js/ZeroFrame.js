2759953c3c9e04d9eb8d6b5e2625ed57d240cf3a
ZeroNet <HelloZeroNet@users.noreply.github.com>
2020-03-29 13:13:47 +0200

    Update ZeroFrame.js

--- template-new/js/ZeroFrame.js
+++ template-new/js/ZeroFrame.js
@@ -1,3 +1,28 @@
+/*
+Original code is Copyright (c) 2020 ZeroNet project.
+Original code was released under the GPLv2 license by ZeroNet project, December 2016.
+Original code was rereleased under the MIT license by ZeroNet project, March 2020.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 // Version 1.0.0 - Initial release
 // Version 1.1.0 (2017-08-02) - Added cmdp function that returns promise instead of using callback
 // Version 1.2.0 (2017-08-02) - Added Ajax monkey patch to emulate XMLHttpRequest over ZeroFrame API
